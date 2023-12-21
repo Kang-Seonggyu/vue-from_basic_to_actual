380dcfd6bf1935692c8b3c452ac7e455f32e2cae
Dev_kang <jokekim609@gmail.com>
2023-12-21 17:21:09 +0900

    fix : 페이지네이션 컴포넌트 화

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -27,55 +27,29 @@
 				></PostItem>
 			</div>
 		</div>
-
-		<nav aria-label="Page navigation example" class="mt-4">
-			<ul class="pagination justify-content-center">
-				<li class="page-item" :class="{ disabled: params._page === 1 }">
-					<a
-						class="page-link"
-						href="#"
-						aria-label="Previous"
-						@click.prevent="--params._page"
-					>
-						<span aria-hidden="true">&laquo;</span>
-					</a>
-				</li>
-				<li
-					v-for="page in pageCount"
-					:key="page"
-					class="page-item"
-					:class="{ active: params._page === page }"
-				>
-					<a
-						class="page-link"
-						href="page"
-						@click.prevent="params._page = page"
-						>{{ page }}</a
-					>
-				</li>
-				<li class="page-item" :class="{ disabled: params._page === pageCount }">
-					<a
-						class="page-link"
-						href="#"
-						aria-label="Next"
-						@click.prevent="++params._page"
-					>
-						<span aria-hidden="true">&raquo;</span>
-					</a>
-				</li>
-			</ul>
-		</nav>
 	</div>
-	<hr class="my-4" />
-	<AppCard>
-		<PostDetailView id="1" />
-	</AppCard>
+	<AppPagination
+		:currentPage="params._page"
+		:page-count="pageCount"
+		@page="
+			page => {
+				params._page = page;
+			}
+		"
+	/>
+	<template v-if="posts && posts.length > 0">
+		<hr class="my-4" />
+		<AppCard>
+			<PostDetailView :id="posts[0].id" />
+		</AppCard>
+	</template>
 </template>
 
 <script setup>
 import PostItem from '@/components/posts/PostItem.vue';
 import PostDetailView from './PostDetailView.vue';
 import AppCard from '@/components/AppCard.vue';
+import AppPagination from '@/components/AppPagination.vue';
 import { getPosts } from '@/api/posts';
 import { useRouter } from 'vue-router';
 import { computed, ref, watchEffect } from 'vue';
