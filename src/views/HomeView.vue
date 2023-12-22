0d51159e51ef98236d2256d91ee7a79d968bacd3
Dev_kang <jokekim609@gmail.com>
2023-12-22 13:26:18 +0900

    minor : HomeView 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,26 @@
 <template>
 	<div>HomeView</div>
+	<p>{{ $route.path }}</p>
+	<p>{{ $route.name }}</p>
+	<button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+	<hr class="my-4" />
+	<AppGrid :items="items" v-slot="{ item }" col-class="col-3">
+		<AppCard>{{ item }}</AppCard>
+	</AppGrid>
 </template>
 
-<script>
-export default {
-	setup() {
-		return {};
-	},
+<script setup>
+import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goAboutPage = () => {
+	router.push('/about');
 };
+
+const items = ref(['Apple', 'Strawberry', 'grape', 'banana']);
 </script>
 
 <style lang="scss" scoped></style>
